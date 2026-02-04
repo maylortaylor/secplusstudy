7b3af585c46b5edbb4645c0c45b1d6d681fb73f3
Matt Taylor <matt.taylor@polestarglobal-usa.com>
2026-02-04 16:18:13 -0500

    adding github pages pipeline

--- secplusstudy/next.config.ts
+++ secplusstudy/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/secplusstudy',
+  assetPrefix: '/secplusstudy/',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
