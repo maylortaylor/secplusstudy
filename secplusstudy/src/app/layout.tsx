d23974facc41e33ac6e688f2aa3c629bc63968df
Matt Taylor <matt.taylor@polestarglobal-usa.com>
2026-02-04 17:06:01 -0500

    feat: Add favicon link to layout.tsx

--- secplusstudy/src/app/layout.tsx
+++ secplusstudy/src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/secplusstudy/favicon/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Roboto+Mono:wght@400;500;600;700&display=swap" rel="stylesheet" />
